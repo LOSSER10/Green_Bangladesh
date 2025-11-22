f8e18c7bc0577f2c99634273818141c2090a77e4
MD Arafat Alam Leom <mdarafatalamleon10@gmail.com>
2025-11-22 19:32:27 +0600

    added displayCategoryPlants

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const display = (categories) => {
 
     const btnDiv = document.createElement("div");
     btnDiv.innerHTML = `
-      <button class="block w-full text-left py-2 px-3 bg-green-100 hover:bg-green-200 rounded-md my-1 text-2xl font-semibold">
+      <button onclick="loadCategoryPlants('${category.id}')" class="block w-full text-left py-2 px-3 bg-green-100 hover:bg-green-200 rounded-md my-1 text-2xl font-semibold">
         ${category.category_name}
       </button>
     `;
@@ -21,6 +21,7 @@ const display = (categories) => {
     levelContainer.append(btnDiv);
   }
 };
+/////////////////////////////////////////////////////////////
 
 // Load plant API data
 const loadPlants = () => {
@@ -64,6 +65,63 @@ const displayPlants = (plants) => {
     cardContainer.append(card);
   });
 };
+
+
+////////////////////////////////////////////////////////////////////////////
+
+
+
+const loadCategoryPlants = (id) => {
+
+    // Show loading (optional)
+    const cardContainer = document.getElementById("card-container");
+    cardContainer.innerHTML = "<h2 class='text-xl font-bold text-center'>Loading...</h2>";
+
+    fetch(`https://openapi.programming-hero.com/api/category/${id}`)
+        .then(res => res.json())
+        .then(data => {
+            displayCategoryPlants(data.plants);
+        })
+        .catch(err => console.log(err));
+};
+
+
+const displayCategoryPlants = (plants) => {
+  const cardContainer = document.getElementById("card-container");
+  cardContainer.innerHTML = "";
+
+  plants.forEach(plant => {
+    const card = document.createElement("div");
+
+    card.innerHTML = `
+      <div class="card bg-base-100 w-96 shadow-sm">
+          <figure class="px-10 pt-10">
+              <img src="${plant.image}" class="rounded-xl h-48 w-96 object-cover" />
+          </figure>
+
+          <div class="card-body">
+              <h2 class="card-title text-2xl">${plant.name}</h2>
+              <p class="text-[#1F2937] text-xl">${plant.description}</p>
+
+              <div class="card-actions flex justify-between">
+                  <button class="btn btn-primary">${plant.category}</button>
+                 <div>
+                  <p>$<span class="font-bold text-2xl">${plant.price}</span></p>
+                 </div>
+              </div>
+
+              <button class="bg-[#15803D] btn btn-block rounded-3xl">
+                <span class="text-white font-bold text-lg py-3">Add to cart</span>
+              </button>
+          </div>
+      </div>
+    `;
+
+    cardContainer.append(card);
+  });
+};
+
+
 
 
 loadPlants();
